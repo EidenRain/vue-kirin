1b7b5e803c171175b16153767327f90595c2ecec
Steffen.A <Eiden5Rain@gmail.com>
2024-01-13 14:22:20 +0100

    Add tailwind & basic routing

---
+++ src/views/InitialView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-gradient-to-r from-cyan-200 to-cyan-400 flex justify-center items-center h-full">
+    <h1 class="text-5xl">Vue Kirin</h1>
+  </div>
+</template>
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-gradient-to-r from-cyan-200 to-cyan-400 flex justify-center items-center h-full">
+    <h1 class="text-5xl">Page not found.</h1>
+  </div>
+</template>
